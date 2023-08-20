f75ad6c69561c7fd5ce1b74035014f31f72190b3
Carlos Alberto Ruiz <rpruiz33@gmailcom>
2023-08-20 00:42:17 -0300

    anda el equals

--- src/test/Test.java
+++ src/test/Test.java
@@ -14,9 +14,9 @@ public class Test {
 		Circulo c1=new Circulo(p1,25);
 		Circulo c2=new Circulo(p2,25);
 		c2.setRadio(p2);
-		c1.setRadio(p1);
+		c1.setRadio(p2);
 		c2.setOrigen(p2);
-		c1.setOrigen(p1);
+		c1.setOrigen(p2);
 		System.out.println(c1.getRadio());
 		System.out.println(c1.getRadio());
 		System.out.println(c1.equals(c2));
